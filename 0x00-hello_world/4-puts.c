261d97e87282b07c1bc060120cabb21d1f5b8ca3
JastinAG <jastinagaziva@gmail.com>
2022-06-02 14:21:13 -0700

    c programm that prints programming is like biulding a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -9,5 +9,5 @@
 int main(void)
 {
 	puts("\"Programming is like biulding a multilingual puzzle");
-	return (0)
+	return (0);
 }
