11f4a0ce4729ca5c3a00420af1ee0d12e232653b
JastinAG <jastinagaziva@gmail.com>
2022-06-02 15:31:48 -0700

    c program that prints various data types and sizes that the programs ocuupies to run on a commputer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char acharacter;
+	int ainterger;
+	long along;
+	long long alonglong;
+	float afloat;
+
+	printf("size of a char: %lu byte(s)\n)", sizeof(acharacter));
+	pritf("size of an int: %lu byte(s)\n)", sizeof(ainterger));
+	printf("size of a long int: %lu byte(s)\n)", sizeof(along));
+	printf("size of a long long int: %lu byte(s)\n)", sizeof(alonglong));
+	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	return (0);
+}
+
