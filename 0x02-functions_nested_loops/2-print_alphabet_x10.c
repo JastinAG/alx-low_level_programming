a0327f39e8a5832ca5ef577a701854deb541a140
JastinAG <jastinagaziva@gmail.com>
2022-06-07 13:52:29 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+void print_alphabet_x10(void)
+{
+	int i=0;
+	char letter = 'a'
+
+		while(i < 10)
+		{
+			letter = 'a'
+				while(letter <='z')
+				{
+				_putchar(letter);
+				letter++;
+		}
+	_putchar('\n');
+
+	i++;
+		}
+}
+
+
