6c5d849da7e53cd4344570b9f16a4a9e05d68647
JastinAG <jastinagaziva@gmail.com>
2022-06-08 13:27:06 -0700

    program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,68 +1,50 @@
 #include <stdio.h>
-void my_Print(int a, int b, int c, int d);
-
 /**
-* main - main block
-* Description: Print all possible combinations of two two-digit numbers.
-* Numbers should range from 0 to 99.
-* The two numbers should be separated by a space.
-* All numbers should be printed with two digits. 1 should be printed as 01.
-* Combination of numbers must be separated by a comma followed by a space.
-* Combinations of numbers should be printed in ascending order.
-* `00 01` and `01 00` are considered as the same combination.
-* You can only use `putchar` to print to console.
-* You can only use `putchar` up to 8 times.
-* You are not allowed to use any variable of type `char`.
-* Here we did it the Felix Obianozie way
-* Return: 0
-*/
-
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 int main(void)
 {
+	int i, j;
 	int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (i = 0; i < 100; i++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
 			{
-				for (d = '0'; d <= '9'; d++)
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
 				{
-					my_Print(a, b, c, d);
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
+	putchar(10);
+
 	return (0);
-}
-
-/**
-* my_Print - does the printing based on certain conditions
-*@a: first digit
-*@b: second digit
-*@c: third digit
-*@d: fourth digit
-*
-* Return: void
-*/
-
-void my_Print(int a, int b, int c, int d)
-{
-	if (a < c || (a == c && b < d))
-	{
-		putchar(a);
-		putchar(b);
-		putchar(' ');
-		putchar(c);
-		putchar(d);
-		if (a == '9' && b == '8' && c == '9' && d == '9')
-		{
-			putchar('\n');
-		} else
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
 }
