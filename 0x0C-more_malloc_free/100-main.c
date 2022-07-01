c28b7e0c28060d6c6888f86e2d44d6c373b11e10
JastinAG <jastinagaziva@gmail.com>
2022-07-01 04:47:59 -0700

    function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-main.c
+++ 0x0C-more_malloc_free/100-main.c
@@ -1,54 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: void pointer
+ * @old_size: already allocated size
+ * @new_size: new size to allocate
+ * Return: pointer to newly allocated memory or null
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
 {
-  unsigned int i;
+	unsigned char *np;
+	unsigned int a;
 
-  i = 0;
-  while (i < size)
-    {
-      if (i % 10)
-        {
-	  printf(" ");
-        }
-      if (!(i % 10) && i)
-        {
-	  printf("\n");
-        }
-      printf("0x%02x", buffer[i]);
-      i++;
-    }
-  printf("\n");
-}
-
-/**
- * main - check the code in my git accounts.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char *p;
-  int i;
-
-  p = malloc(sizeof(char) * 10);
-  p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-  i = 0;
-  while (i < 98)
-    {
-      p[i++] = 98;
-    }
-  simple_print_buffer(p, 98);
-  free(p);
-  return (0);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	np = malloc(new_size * sizeof(char));
+	if (np == NULL)
+		return (NULL);
+	a = 0;
+	if (new_size > old_size)
+	{
+		while (a < old_size)
+		{
+			np[a] = ((char *)ptr)[a];
+			a++;
+		}
+		free(ptr);
+		return (np);
+	}
+/* if new_size < old_size */
+	while (a < new_size)
+	{
+		np[a] = ((char *)ptr)[a];
+		a++;
+	}
+	free(ptr);
+	return (np);
 }
